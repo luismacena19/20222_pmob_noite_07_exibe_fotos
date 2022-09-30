ad30a72d94b21f74602ee5fb6636c237d5e93f1d
Luis Macena <luis.macena@outlook.com>
2022-09-30 18:56:52 -0300

    appostila07, fim da sessão 2.3

---
+++ src/components/Busca.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+export default class Busca extends Component {
+    state = {
+        termoDeBusca: ""
+    }
+    onTermoAlterado = (event) => {
+        console.log(event.target.value)        
+    }
+  render() {
+    return (
+      //.flex.flex-column
+        <div className="flex flex-column">
+            {/* span.p-input-icon-left.w-full */}
+            <span className="p-input-icon-left w-full">
+                {/* i.pi.pi-search */}
+                <i className="pi pi-search"></i>
+                <InputText
+                className='w-full'
+                placeholder={this.props.dica}
+                onChange={this.onTermoAlterado}/>
+            </span>
+            <Button 
+             label="OK"
+             className="p-button-outlined mt-2"
+            />
+
+        </div>
+    )
+  }
+}
+
+Busca.defaultProps= {
+    dica: "Digite algo que você deseja ver..."
+} 
